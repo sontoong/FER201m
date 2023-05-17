7de0ae4be3581456b975a2b0efbab8ee7f0744f8
MT <vominhthang061003@gmail.com>
2023-05-17 20:02:11 +0700

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const footer = () => {
+  return <div className="footer">copyright © 2022</div>;
+};
+
+export default footer;
---
+++ src/components/Navigation.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import "../App.css";
+
+export default class Navigation extends Component {
+  render() {
+    return (
+      <div className="NavBar">
+        <nav>
+          <ul>
+            <li>
+              <a className="active" href="#home">
+                Home
+              </a>
+            </li>
+            <li>
+              <a href="#news">News</a>
+            </li>
+            <li>
+              <a href="#about">About</a>
+            </li>
+            <li>
+              <a href="#contact">Contact</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    );
+  }
+}
