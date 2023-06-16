98e42d37607626eb24f47a19daf25469623c85fa
MT <vominhthang061003@gmail.com>
2023-06-16 23:40:09 +0700

    Scss

--- src/App.js
+++ src/App.js
@@ -1,30 +1,12 @@
-import React from 'react';
-import { Route, Routes } from 'react-router-dom';
-import Navigation from './Components/Navigation';
-import Footer from './Components/Footer';
-import Player from './Components/Player';
-import Detail from './Components/Detail';
-import Contact from './Components/Contact';
-import About from './Components/About';
+import './App.scss';
+import Card from './Components/Card';
 
-
-
-const App = () => {
+function App() {
   return (
-    <div className='App'>
     <div>
-      <Navigation/>
-      <Routes>
-          <Route path='/' element={<Player/>}> 
-      </Route>
-        <Route path='/about' element={<About/>}></Route>
-        <Route path='/detail/:id' element={<Detail/>}></Route>
-        <Route path='/contact' element={<Contact/>}></Route>
-      </Routes>
-      <Footer/>
+      <Card/>
     </div>
-    </div>
-  )
+  );
 }
 
-export default App
+export default App;
